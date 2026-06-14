881bf43f93625ac1e8bbffcd66a811760e792bf3
Hadityaa <mugiwara0640@gmail.com>
2026-06-14 18:21:57 +0700

    ubahh semuanyaa

--- TubesDPBO/src/main/java/view/DashboardAdmin.java
+++ TubesDPBO/src/main/java/view/DashboardAdmin.java
@@ -34,11 +34,11 @@ public class DashboardAdmin extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 708, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 423, Short.MAX_VALUE)
         );
 
         pack();
